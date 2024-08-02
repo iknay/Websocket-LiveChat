d62a230e55062e927a185885c6a875fcc5697302
Yancee Villanueva <yanceevillanueva16@gmail.com>
2024-08-02 22:51:03 +0800

    fix

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -4,5 +4,5 @@ import { io } from "socket.io-client";
 
 export const socket = io("https://websocket-live-chat-server.vercel.app", {
   withCredentials: true,
-  transports: ["websocket"],
+  // transports: ["websocket"],
 });
