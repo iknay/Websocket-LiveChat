0ae05ef69673d94e3106a9a0a4fc8ec774a72760
Yancee Villanueva <yanceevillanueva16@gmail.com>
2024-08-02 22:20:01 +0800

    Fix

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -3,5 +3,6 @@ import { io } from "socket.io-client";
 // "undefined" means the URL will be computed from the `window.location` object
 
 export const socket = io("https://websocket-live-chat-server.vercel.app", {
+  transports: ["websocket", "polling"],
   withCredentials: true,
 });
